bbda39366f5702e1c9b9ce86d177d362c0a4d1ca
Gihun Ham <progh2@gmail.com>
2025-06-13 10:03:15 +0900

    상속 관계에서의 생성자 호출 예

--- Program.cs
+++ Program.cs
@@ -8,8 +8,36 @@ namespace ClassExample31
 {
     internal class Program
     {
+        class Parent
+        {
+            public Parent() { Console.WriteLine("Parent()"); }
+            public Parent(string name) { Console.WriteLine("Parent(string name)"); }
+            public Parent(float param) { Console.WriteLine("Parent(double param)"); }
+        }
+        class Child : Parent
+        {
+            public Child() : base("child")
+            {
+                Console.WriteLine("Child()"); 
+            }
+            public Child(string name) : base(name) { 
+                Console.WriteLine("Child(name)");
+            }
+            public Child(float param) : base(param)
+            {
+                Console.WriteLine("Child(dobule param)");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Child childA = new Child("abc");
+            Child childB = new Child(3L);
+            // int < long < float < double 
+            //  4     8      4      8
         }
+
+
+
     }
 }
